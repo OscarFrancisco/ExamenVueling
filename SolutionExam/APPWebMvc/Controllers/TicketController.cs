2b1474d3c900db10bf8fa75bcb8b874e8d3ff439
OscarFrancisco <theoscarf@hotmail.com>
2016-05-07 14:28:28 +0200

    Commit

---
+++ SolutionExam/APPWebMvc/Controllers/TicketController.cs
@@ -0,0 +1,76 @@
+﻿using Domain;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APPWebMvc.Controllers
+{
+    public class TicketController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            HttpClient _httpclient = new HttpClient();
+            var task = await _httpclient.GetAsync("http://localhost:1228/ServiceArticle.svc/");
+            var jsonString = await task.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<Article>(jsonString);
+            return View(model);
+        }
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
